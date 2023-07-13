203aa9c731351987ed03e1f17fb39c3d66879ae3
Kalil Mendes <102493336+lilmendes@users.noreply.github.com>
2023-07-12 22:11:04 -0300

    Add files via upload

---
+++ ATIVIDADE 22 DO WHILE/src/pacote/Atv22.java
@@ -0,0 +1,46 @@
+package pacote;
+
+public class Atv22 {
+
+	public static void main(String[] args) {
+		//Problema: elaborar uma sequencia de valores inteiros, positivos,
+		//ímpares iniciada pelo número 1, contendo 20 elementos. 
+		//Utilize a estrutura do/while para solucionar esse problema.
+		int num = 1, qtd = 1; 
+
+
+        do {  
+       if (num % 2 != 0) { 
+    		  System.out.print(num + (" "));
+    		  qtd++;
+    		  } 	
+   
+       num++;
+       } while(qtd<=20);
+        
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
